d8fdd5d612fe1f807547fc74b8bf39727a092a1d
NoahFrahm <nofrahm@live.unc.edu>
2022-08-28 15:55:07 -0400

    fixed command line input processing
    fixed minimist process on args to take in correct port num

--- server.js
+++ server.js
@@ -8,31 +8,27 @@ const http = require('fs')
 const http = require('minimist')
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-var args = process.argv.slice(2);
+const args = process.argv.slice(2);
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args['-port='] || 3000
+const port = args.port || 3000
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
 var filePath = `./public/index.html`
-var fileData = fs.readFile(filename, (err, data) => {
+var fileData = fs.readFile(filename, 'utf8', (err, data) => {
     if (err) {
-        console.error('file couldnt be read')
-        return
+      console.error(err);
+      return;
     }
-    return data
- })
-
-
+    console.log(data);
+  });
+  
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
-
-
-
 
 
 // Define a const `server` as an arrow function using http.createServer. 
